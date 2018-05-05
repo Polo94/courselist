cff9099c7467c84ada613c6529cda255fff3ae70
Polo94 <paul.henry.claudant@gmail.com>
2018-05-05 16:23:12 +0200

    creation and registration of products + views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,26 @@
 class ProductsController < ApplicationController
+
   def index
+    @products = Product.all.order("created_at DESC")
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(params.require(:product).permit(:title, :description, :quantity))
+    if @product.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:title, :description, :quantity)
+    end
+
 end
