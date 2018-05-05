189baeac1f1a90bd49eadf8d5bd92c2adf27a53b
Polo94 <paul.henry.claudant@gmail.com>
2018-05-05 17:00:39 +0200

    edit & show of products + views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,13 @@
 class ProductsController < ApplicationController
 
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @products = Product.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -17,10 +23,30 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+
+
   private
 
     def product_params
       params.require(:product).permit(:title, :description, :quantity)
     end
 
+    def find_product
+      @product = Product.find(params[:id])
+    end
 end
